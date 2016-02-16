498b28f8462f3983382d0eaa22edef3199d33298
Nathan Johnson <njohnson@ena.com>
2016-02-16 11:12:28 -0600

    now with logging

--- enaipamdriver/main.go
+++ enaipamdriver/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"github.com/docker/go-plugins-helpers/ipam"
 	"github.com/docker/libkv/store"
 	"github.com/docker/libnetwork/datastore"
+	"os"
 	"stash.corp.ena.net/rd/ena-ipamdriver.git/driver"
 	"time"
 )
@@ -14,6 +16,17 @@ const APP_VERSION = "0.1"
 
 // The flag package provides a default help printer via -h switch
 var versionFlag *bool = flag.Bool("v", false, "Print the version number.")
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stderr instead of stdout, could also be a file.
+	log.SetOutput(os.Stderr)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.DebugLevel)
+}
 
 func main() {
 	flag.Parse() // Scan the arguments list
